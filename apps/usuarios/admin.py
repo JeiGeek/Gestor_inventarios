1484e9a33459d27734729c606c28909d76d794bb
Jeison Guarguati <jeisonensp03@gmail.com>
2025-09-21 09:04:24 -0500

    C2: Configuracion de django manager

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuario, Rol
 
-# Register your models here.
+# Registro básico
+admin.site.register(Usuario)
+admin.site.register(Rol)
+
+# Personalización del sitio de administración
+admin.site.site_header = "Administración de Usuarios"
+admin.site.site_title = "Panel de Control"
+admin.site.index_title = "Bienvenido al Panel de Control"
