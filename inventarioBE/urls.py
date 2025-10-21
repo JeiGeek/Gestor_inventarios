df5c3dd0e22f78085947291ca418aae34456e6b6
LauraRosas10 <lalavanesarosas@gmail.com>
2025-10-20 22:29:32 -0500

    Recuperacion de contraseña

--- inventarioBE/urls.py
+++ inventarioBE/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
 
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    #Recuperar contraseña
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 
     # Google OAuth
